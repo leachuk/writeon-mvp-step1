bfea9de497908c3844a2d19ac65ca6522303ad93
leachuk <stew.leach@gmail.com>
2014-12-31 17:43:05 +1100

    Consuming promise object to extract model data from server

--- client/app/controllers/createArticle/createArticle.controller.js
+++ client/app/controllers/createArticle/createArticle.controller.js
@@ -5,15 +5,10 @@ angular.module('writeonMvpStep1App')
     $scope.article = {
     	bodyText :''
     };
-    //console.log("authenticationService: " + authenticationService.someMethod());
-    //console.log(API.someMethod());
 
-    //console.log(API);
-    // API.Article.getArticle({id:'12345'}, function(data){
-    //     $scope.model = new Article(data);
-    //     //console.log($scope.model);
-    // });
-
-    console.log(API.Article.getArticle('UserArticle','54321'));
+    API.Article.getArticle('UserArticle','54321').then(function(data){
+        $scope.model = new Article(data);
+        console.log(data);
+    });
 
   });
