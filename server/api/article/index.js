d83f9a282b8a37c5818d0ac3586fe91d2d82bf7f
leachuk <stew.leach@gmail.com>
2015-04-15 21:09:06 +1000

    adding example acl methods

--- server/api/article/index.js
+++ server/api/article/index.js
@@ -2,11 +2,32 @@
 
 var express = require('express');
 var controller = require('./article.controller');
-
 var router = express.Router();
 
+var acl = require('acl');
+acl = new acl(new acl.memoryBackend()); //TODO: Update to Redis backend
+acl.addUserRoles('testuser', 'guest')
+acl.allow('guest', 'articles', ['edit','view']);
+
 router.get('/', controller.index);
-router.get('/getarticle/:id', controller.getArticle);
+router.get('/getarticle/:id', function(req, res, next){
+	acl.isAllowed('testuser', 'articles', ['view'], function(err, res){
+	    if(res){
+	        console.log("User member is allowed to view articles");
+	        console.log(res);
+	    } else {
+	    	console.log("error");
+	    	console.log(err);
+	    }
+	});
+
+	//get permissions
+	acl.allowedPermissions('testuser', ['articles'], function(err, permissions){
+    	console.log(permissions)
+	});
+
+	next();
+}, controller.getArticle);
 router.get('/listAllUserArticles/:username', controller.listAllUserArticles);
 router.post('/saveArticle', controller.saveArticle);
 router.post('/updateArticle', controller.updateArticle);
