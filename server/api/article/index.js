f077d87863621259a748d48840f9717daa4bb9c4
leachuk <stew.leach@gmail.com>
2015-04-21 23:15:26 +1000

    continuing authorisation code

--- server/api/article/index.js
+++ server/api/article/index.js
@@ -6,14 +6,25 @@ var express = require('express');
 var controller = require('./article.controller');
 var router = express.Router();
 
-var aclService = require('server/services/auth/auth.controller').AuthService;
-aclService.initAuthorization();
-var acl = aclService.acl();
+var authService = require('server/services/auth/auth.controller').AuthService;
+authService.initAuthorization();
+var acl = authService.acl();
 
 router.get('/', controller.index);
 router.get('/getarticle/:id', function(req, res, next){
+	var username = null;
+	authService.fulldecodetoken(req, res, function(err, result){
+		if(result){
+			console.log("fulldecodetoken result");
+			console.log(result);
+			username = result.username;
+		} else {
+			console.log("fulldecodetoken error");
+			console.log(err);
+		}
+	});
 
-	acl.isAllowed('testuser', 'articles', ['view'], function(err, res){
+	acl.isAllowed(username, 'getarticle', ['view'], function(err, res){
 	    if(res){
 	        console.log("User member is allowed to view articles");
 	        console.log(res);
@@ -23,8 +34,8 @@ router.get('/getarticle/:id', function(req, res, next){
 	    }
 	});
 	next();
-
 }, controller.getArticle);
+//router.get('/getarticle/:id', acl.middleware(), controller.getArticle);
 router.get('/listAllUserArticles/:username', controller.listAllUserArticles);
 router.post('/saveArticle', controller.saveArticle);
 router.post('/updateArticle', controller.updateArticle);
--- server/services/auth/auth.controller.js
+++ server/services/auth/auth.controller.js
@@ -16,9 +16,47 @@ AuthService.prototype.decodetoken = function(secret, token, callback) {
   callback(decoded);
 };
 
+AuthService.prototype.fulldecodetoken = function(req, res, callback) {
+  console.log("in decodefulltoken");
+  var err = null;
+  var result = null;
+  var token = null;
+  var parts = req.headers.authorization.split(' ');
+ 
+  if (parts.length == 2) {
+    var scheme = parts[0];
+    var credentials = parts[1];
+
+    if (/^Bearer$/i.test(scheme)) {
+        token = credentials;
+        result = jwt.decode(token, req.secret);
+  		callback(err,result);  
+    } else {
+      console.log("Invalid Authorization Header. Format is Authorization: Bearer [token]");
+      err = new Error('Invalid Authorization Header. Format is Authorization: Bearer [token]');
+      callback(err,result);
+    }
+  } else {
+    console.log("Format is Authorization: Bearer [token]");
+    err = new UnauthorizedError('credentials_bad_format', {
+      message : 'Format is Authorization: Bearer [token]'
+    });
+    callback(err,result);
+  }
+};
+
 AuthService.prototype.initAuthorization = function() {
-	acl.addUserRoles('testuser', 'guest')
-	acl.allow('guest', 'articles', ['edit','view']);
+	//acl.addUserRoles('testuser', 'guest')
+	//acl.allow('guest', 'articles', ['edit','view']);
+
+	//create roles
+	//TODO refactor this into a proper model/schema with jugglingDb
+	acl.allow('article-editor','getarticle',['edit', 'view', 'delete']);
+	acl.allow('article-viewer','getarticle',['view']);
+
+	//assign users to roles
+	acl.addUserRoles('writeonmvpstep1-1@test.com', 'article-editor');
+	acl.addUserRoles('writeonmvpstep1-3@test.com', 'article-viewer');
 };
 
 AuthService.prototype.acl = function() {
