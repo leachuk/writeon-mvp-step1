c047165659c85e7b41424c02caf117f69a8cbc92
leachuk <stew.leach@gmail.com>
2015-05-10 23:03:16 +1000

    Adding access control headers to enable CORS for remote API calls

--- server/config/express.js
+++ server/config/express.js
@@ -39,6 +39,21 @@ module.exports = function(app) {
                                                                    new RegExp('/api/articles/.*'),//temp non secured
                                                                    '/services/couchDbHandler']}));
   
+  // Add headers to allow remote API calls
+  app.use(function (req, res, next) {
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      // Pass to next layer of middleware
+      next();
+  });
+
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
