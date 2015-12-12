b12d102f84e49c31209e882690215ebe22348ef5
stewart leach <stewartleach@stu-mbp.local>
2015-12-12 12:02:08 +1100

    adding paybracket to model

--- server/models/RecruitUnit.Job.All.js
+++ server/models/RecruitUnit.Job.All.js
@@ -7,18 +7,20 @@ console.log("In RecruitUnitJobItem model");
 var initCookie = function (setcookie, options) {
     // Initialize here
     cookie = setcookie;
-    schema = new Schema('nano', {url: config.couchuri + '/article_documents', cookie: cookie}); //set couchdb documents directory
+    schema = new Schema('nano', {url: config.couchuri + '/' + config.dbNameArticles, cookie: cookie}); //set couchdb documents directory
     console.log("cookie:" + cookie);
     console.log("options:");
     console.log(options);
 
     var returnModelObj = {
 		id: 					{ type: String},
-		model: 					{ type: String}, 
+		model: 					{ type: String},
 	    jobDesc:     			{ type: String, length: 255 },
 	    roleType:   			{ type: String }, //Text is used for large strings
-	    location: 				{ type: String}, 
-	    requiredSkills: 		{ type: String}, 
+      payBracketLower:  { type: Number},
+      payBracketUpper:  { type: Number},
+	    location: 				{ type: String},
+	    requiredSkills: 		{ type: String},
 	    authorName: 			{ type: String },
 	    authorEmail:    		{ type: String},
 	    createdDate: 			{ type: Number,  default: Date.now },
@@ -26,7 +28,7 @@ var initCookie = function (setcookie, options) {
 	    lastUpdatedDate: 		{ type: Number,  default: Date.now },
 	    lastUpdatedDateFormatted:{ type: String},
 	    published: 	 			{ type: Boolean, default: false, index: true }
-	} 
+	}
 
 	console.log("returnModelObj");
 	console.log(returnModelObj);
