21cf2b67a2fa224dcc7819c417dd676eb5478b51
stewart leach <stewart.leach@originenergy.com.au>
2017-05-06 15:48:57 +1000

    adding signup validation middleware

--- server/services/recruitunit/validation/user.js
+++ server/services/recruitunit/validation/user.js
@@ -1,10 +1,13 @@
 'use strict';
 var Joi = require('joi');
+var errorMessage400 = "Invalid request parameters";
 
-var schema = Joi.object().keys({
-  username: Joi.string().min(3).required(),
-  password: Joi.string().min(3).required(),
-  email: Joi.string().email().required()
+var signUpSchema = Joi.object().keys({
+  email: Joi.string().email().max(100).required(),
+  jobRole: Joi.string().valid(['developer','recruiter']).required(),
+  displayName: Joi.string().alphanum().min(3).max(100).required(),
+  password: Joi.string().min(3).max(100).required(),
+  key: Joi.string().min(3).max(100).required()
 });
 
 function UserValidationService(){};
@@ -12,9 +15,15 @@ function UserValidationService(){};
 UserValidationService.prototype.signup = function(){
   return function(req, res, next) {
     console.log("UserValidationService: signup");
-    console.log(req.body);
 
-    return next();
+    Joi.validate(req.body, signUpSchema, function (err, value) {
+      if (!err){
+        next();
+      } else {
+        console.log("Validation error: " + err.message);
+        return(res.status(400).send(new Error(errorMessage400).message));
+      }
+    });
   }
 }
 
