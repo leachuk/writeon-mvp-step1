4b00d50652e18bcd6964c316b09cd9ed51ba8ace
stewart leach <stewartleach@Naya-Las-MacBook-Pro.local>
2017-08-07 20:01:47 +1000

    removing bardly.net domain as it doesn't appear to be setup

--- letsencrypt/certbot-create.sh
+++ letsencrypt/certbot-create.sh
@@ -6,14 +6,14 @@ GetCert() {
         docker run \
           -v $(pwd)/web-volume/letsencrypt/nginx-certs:/etc/letsencrypt \
           -e http_proxy=$http_proxy \
-          -e domains="bardly.net, www.bardly.net" \
+          -e domains="www.bardly.net" \
           -e email="stewartleachuk@hotmail.com" \
           -p 80:80 \
           -p 443:443 \
           --rm pierreprinetti/certbot:latest
 }
 
-echo "Getting certificates for bardly.net and www.bardly.net..."
+echo "Getting certificates for www.bardly.net..."
 GetCert
 
 echo "Rebuilding frontend docker with certs..."
